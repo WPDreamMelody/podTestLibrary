f6e757a79bdb300d7dd5164b47ab8063c10c7309
kyu@Ctrip.com <Qaz~1234>
2015-08-31 17:49:47 +0800

    Initial Commit of Library
    Signed-off-by: kyu@Ctrip.com <Qaz~1234>

---
+++ Example/Pods/Headers/Private/podTestLibrary/UILabel+Iconkit.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/UILabel+Iconkit.h
---
+++ Example/Pods/Target Support Files/podTestLibrary/podTestLibrary-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "UILabel+Iconkit.h"
+
+FOUNDATION_EXPORT double podTestLibraryVersionNumber;
+FOUNDATION_EXPORT const unsigned char podTestLibraryVersionString[];
+
